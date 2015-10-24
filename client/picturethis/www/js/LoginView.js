dbcde8dfbed1cc68afb763bbd363d71593cdcf79
Brian Chau <me@brianchau.ca>
2015-10-23 17:45:20 -0700

    sample code for connecting to servr

--- client/picturethis/www/js/LoginView.js
+++ client/picturethis/www/js/LoginView.js
@@ -12,7 +12,22 @@ var LoginView = function (service) {
 	};
 
 	this.userLogin = function(username,password) {
-		showAlert("Name: " + username + " Pass: " + password, "Login");
+        var xmlhttp;
+        if (window.XMLHttpRequest){
+            xmlhttp = new XMLHttpRequest();
+        }
+        else {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xmlhttp.open('GET', 'http://picturethis.brianchau.ca/api/login/login?username=' + username + '&password=' + password, true);
+        xmlhttp.send();
+        console.log(xmlhttp);
+        console.log(xmlhttp.responseText);
+        if (xmlhttp.responseText != "undefined"){
+            var obj = JSON.parse(xmlhttp.responseText);
+            showAlert(obj);
+        }
 	};
 
 	this.initialize();
