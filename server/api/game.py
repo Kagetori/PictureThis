063eb9e672364f5c96149ba61090b34227103522
Karen Guo <karenguo@live.com>
2015-10-25 12:59:33 -0700

    Added models for word prompt and turn

--- server/api/game.py
+++ server/api/game.py
@@ -1,7 +1,7 @@
-from models import Game, User
+from models import Game, User, Turn, WordPrompt
 
 from interface.exception import RemoteException
-from interface.user import Game as LocalGame
+from interface.game import Game as LocalGame
 
 import string
 import utility
@@ -29,9 +29,30 @@ def start_new_game(user_id, friend_id):
 		friend = User.objects.get(id=friend_id)
 	except User.DoesNotExist:
 		return RemoteException("User 2 doesn't exist")
-
+	# should also check no active game between user 1 and user 2
 	game = Game.objects.create(user_id1=user_id, user_id2=friend_id, active=True, curr_round=1)
 	game.save()
 
-	return LocalGame(user_id=user_id, friend_id=friend_id, active=True, curr_round=1)
+	return LocalGame(user_id=user_id, friend_id=friend_id, active=True, curr_round=1, words_seen=[])
 
+def new_round(user_id, game_id):
+	"""
+	Starts a new round by giving user_id a new word prompt 
+	"""
+	if user_id is None or game_id is None:
+		return RemoteException('User ID and game ID cannot be blank.')
+	try:
+		user = User.objects.get(id=user_id)
+	except User.DoesNotExist:
+		return RemoteException("User does not exist")
+
+	try:
+		game = Game.objects.get(id=game_id)
+	except Game.DoesNotExist:
+		return RemoteException("Game does not exist")
+
+	if (game.active == false):
+		return RemoteException("Game is inactive")
+	# TODO start a new round
+
+	
---
+++ server/interface/game.py
@@ -0,0 +1,19 @@
+from remote_object import RemoteObject
+
+class Game(RemoteObject):
+    def __init__(self, user_id, friend_id, active, curr_round, words_seen):
+        """
+        user_id
+            user's id
+        friend_id
+            friend's id
+        active
+            is game active?
+        curr_round
+            number of rounds played
+        """
+        self.user_id = user_id
+        self.friend_id = friend_id
+        self.active = active
+        self.curr_round = curr_round
+        self.words_seen = words_seen
