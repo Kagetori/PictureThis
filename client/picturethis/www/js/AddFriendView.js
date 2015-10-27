d7dfdaa4d590fab220633fca5ab9bff0cc576ae0
yukiwong <yukiwong_ky@hotmail.com>
2015-10-27 12:48:04 -0700

    addfriend works

--- client/picturethis/www/js/AddFriendView.js
+++ client/picturethis/www/js/AddFriendView.js
@@ -13,31 +13,50 @@ var AddFriendView = function (service) {
 
 	//queries server with friend username. Adds as friend if username found, throws exception otherwise
 	this.findFriend = function(username) {
+		showAlert("got to findFriend!");
+		showAlert("username: " + username);
 		var url = 'http://picturethis.brianchau.ca/api/search/find_user?username=' + username;
+
 		var searchParser = function(result) {
-			   var obj = JSON.parse(result);
-               if (typeof obj.exception === "undefined") {
+		   var obj = JSON.parse(result);
+		   		if (typeof obj.exception === "undefined") {
 					var friendUsername = obj.username;
 					var friend_id = obj.user_id;
 					//call add friend - they currently don't have a choice
-					this.addFriend(friend_id);
+					showAlert("Your friend " + friendUsername + " has been found!");
+					var addFriendView = new AddFriendView();
+					addFriendView.addFriend(friend_id);
 
 				} else {
-						//shows exception message
-						showAlert(obj.exception);
-						};
+					//shows exception message
+					showAlert(obj.exception);
+					};
+			};
 
 		var serverCaller = new ServerCaller(url,searchParser);
-		};
+
 	};
 
 	//queries server with self id and friend id. Updates friends field for user(?)
 	this.addFriend = function(friend_id) {
-		var user = getUser();
-		var user_id = user.id;
-		var url = 'http://picturethis.brianchau.ca/api/friend/add_friend?user_id=' + user_id + '&target_id=' + friend_id;
-		//add friendParser that parses whatever comes back into friend objects and stores them in friends field in user?
-	}
+    		var user = getUser();
+    		var user_id = user.id;
+    		var url = 'http://picturethis.brianchau.ca/api/friend/add_friend?user_id=' + user_id + '&target_id=' + friend_id;
+    		var addFriendParser = function(result) {
+    			var obj = JSON.parse(result);
+    			   if (typeof obj.exception === "undefined") {
+    					user.friends = obj.friends;
+    					showAlert("Friend added!");
+
+    				} else {
+    					//shows exception message
+    					showAlert(obj.exception);
+    					};
+    		};
+
+    		var serverCaller = new ServerCaller(url,addFriendParser);
+
+    	};
 
 	this.backToMain = function() {
 		window.location="index2.html";
