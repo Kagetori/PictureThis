dbfdc73af3f01c7e3faa45183a43cb9baef065c1
Brian Chau <me@brianchau.ca>
2015-10-30 08:38:08 +0100

    write a few more tests for login and friend

--- server/api/tests.py
+++ server/api/tests.py
@@ -6,7 +6,7 @@ from interface.exception import RemoteException
 from interface.success import SuccessPacket
 from interface.packets import GamePacket
 
-import login, search, game
+import login, friend, search, game
 
 # Create your tests here.
 
@@ -35,6 +35,70 @@ class LoginTests(TestCase):
         # Test duplicate create user failing
         self.assertRaises(RemoteException, login.create_user, username='user1', password='pw1')
 
+    def testLogin(self):
+        # Make a few users first
+        usernames = []
+        passwords = []
+
+        for i in range(5):
+            username = 'user' + str(i)
+            password = 'pw' + str(i)
+
+            usernames.append(username)
+            passwords.append(password)
+
+            login.create_user(username=username, password=password)
+
+        # Try loggin them in
+        for i in range(5):
+            username = usernames[i]
+            password = passwords[i]
+
+            # Try logging in with incorrect or empty password
+            self.assertRaises(RemoteException, login.login, username=username, password='asdf')
+            self.assertRaises(RemoteException, login.login, username=username, password=None)
+
+            user_view = login.login(username=username, password=password)
+
+            user = User.objects.get(name=username)
+
+            self.assertEqual(user_view.username, user.name)
+            self.assertEqual(user_view.user_id, user.obfuscated_id)
+
+
+class FriendTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(5):
+            username = 'user' + str(i)
+            password = 'pw' + str(i)
+            login.create_user(username=username, password=password)
+
+    def testFriendApi(self):
+        user0 = User.objects.get(name='user0')
+        user0_id = user0.obfuscated_id
+
+        for i in range(1, 5):
+            username = 'user' + str(i)
+            user_id = User.objects.get(name=username).obfuscated_id
+
+            friend.add_friend(user_id=user0_id, target_id=user_id)
+
+            self.assertTrue(friend.is_friend(user_id1=user0_id, user_id2=user_id))
+            self.assertTrue(friend.is_friend(user_id1=user_id, user_id2=user0_id))
+
+        user0_friends = friend.get_user_friends(user_id=user0_id)
+
+        self.assertEqual(len(user0_friends.friends), 4)
+
+        for i in range(1, 5):
+            username = 'user' + str(i)
+            user_id = User.objects.get(name=username).obfuscated_id
+
+            friend.remove_friend(user_id=user0_id, target_id=user_id)
+
+            self.assertFalse(friend.is_friend(user_id1=user0_id, user_id2=user_id))
+            self.assertFalse(friend.is_friend(user_id1=user_id, user_id2=user0_id))
 
 
 class SearchTests(TestCase):
@@ -68,7 +132,6 @@ class GameTests(TestCase):
             password = 'pw' + str(i)
             login.create_user(username=username, password=password)
 
-        return
 
     def testStartNewGame(self):
         user1_id = User.objects.get(name='user1').obfuscated_id
