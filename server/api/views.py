429a4d3aaa16d85ebc0f39b1e3462ccd5264fb11
Brian Chau <me@brianchau.ca>
2015-10-30 19:58:46 -0700

    server-side decode of encodeURIComponent

--- server/api/views.py
+++ server/api/views.py
@@ -5,28 +5,30 @@ from api import friend, game, login, poll, search
 from interface.success import SuccessPacket
 from interface.exception import RemoteException
 
+import urllib
+
 # FRIEND API
 
 def friend__add_friend(request):
     params = _params(request)
     
-    user_id = params.get('user_id', 0)
-    friend_id = params.get('friend_id', 0)
+    user_id = _get_param(params, 'user_id', 0)
+    friend_id = _get_param(params, 'friend_id', 0)
 
     return _response(friend.add_friend, user_id=user_id, friend_id=friend_id)
 
 def friend__remove_friend(request):
     params = _params(request)
     
-    user_id = params.get('user_id', 0)
-    friend_id = params.get('friend_id', 0)
+    user_id = _get_param(params, 'user_id', 0)
+    friend_id = _get_param(params, 'friend_id', 0)
 
     return _response(friend.remove_friend, user_id=user_id, friend_id=friend_id)
 
 def friend__get_friends(request):
     params = _params(request)
     
-    user_id = params.get('user_id', 0)
+    user_id = _get_param(params, 'user_id', 0)
 
     return _response(friend.get_user_friends, user_id=user_id)
 
@@ -35,20 +37,20 @@ def friend__get_friends(request):
 def login__create_user(request):
     params = _params(request)
     
-    username = params.get('username', None)
-    password = params.get('password', None)
-    client_version = params.get('client_version', 0)
-    device_id =  params.get('device_id', None)
+    username = _get_param(params, 'username', None)
+    password = _get_param(params, 'password', None)
+    client_version = _get_param(params, 'client_version', 0)
+    device_id =  _get_param(params, 'device_id', None)
 
     return _response(login.create_user, username=username, password=password, client_version=client_version, device_id=device_id)
 
 def login__login(request):
     params = _params(request)
     
-    username = params.get('username', None)
-    password = params.get('password', None)
-    client_version = params.get('client_version', 0)
-    device_id =  params.get('device_id', None)
+    username = _get_param(params, 'username', None)
+    password = _get_param(params, 'password', None)
+    client_version = _get_param(params, 'client_version', 0)
+    device_id =  _get_param(params, 'device_id', None)
 
     return _response(login.login, username=username, password=password, client_version=client_version, device_id=device_id)
 
@@ -58,7 +60,7 @@ def login__login(request):
 def poll__update(request):
     params = _params(request)
 
-    user_id = params.get('user_id', 0)
+    user_id = _get_param(params, 'user_id', 0)
 
     return _response(poll.update, user_id=user_id)
 
@@ -69,7 +71,7 @@ def poll__update(request):
 def search__find_user(request):
     params = _params(request)
 
-    username = params.get('username', None)
+    username = _get_param(params, 'username', None)
 
     return _response(search.find_user, username=username)
 
@@ -79,24 +81,24 @@ def search__find_user(request):
 def game__start_new_game(request):
     params = _params(request)
 
-    user_id = params.get('user_id', 0)
-    friend_id = params.get('friend_id', 0)
+    user_id = _get_param(params, 'user_id', 0)
+    friend_id = _get_param(params, 'friend_id', 0)
 
     return _response(game.start_new_game, user_id=user_id, friend_id=friend_id)
 
 def game__send_picture(request):
     params = _params(request)
 
-    user_id = params.get('user_id', None)
-    game_id = params.get('game_id', None)
+    user_id = _get_param(params, 'user_id', None)
+    game_id = _get_param(params, 'game_id', None)
 
     return _response(game.send_picture, user_id=user_id, game_id=game_id)
 
 def game__get_picture(request):
     params = _params(request)
 
-    user_id = params.get('user_id', None)
-    game_id = params.get('game_id', None)
+    user_id = _get_param(params, 'user_id', None)
+    game_id = _get_param(params, 'game_id', None)
 
     try:
         return game.get_picture(user_id=user_id, game_id=game_id)
@@ -106,24 +108,24 @@ def game__get_picture(request):
 def game__end_game(request):
     params = _params(request)
 
-    user_id = params.get('user_id', None)
-    game_id = params.get('game_id', None)
+    user_id = _get_param(params, 'user_id', None)
+    game_id = _get_param(params, 'game_id', None)
 
     return _response(game.end_game, user_id=user_id, game_id=game_id)
 
 def game__validate_guess(request):
     params = _params(request)
 
-    user_id = params.get('user_id', None)
-    game_id = params.get('game_id', None)
-    guess = params.get('guess', None)
+    user_id = _get_param(params, 'user_id', None)
+    game_id = _get_param(params, 'game_id', None)
+    guess = _get_param(params, 'guess', None)
 
     return _response(game.validate_guess, user_id=user_id, game_id=game_id, guess=guess)
 
 def game__get_user_games(request):
     params = _params(request)
 
-    user_id = params.get('user_id', None)
+    user_id = _get_param(params, 'user_id', None)
 
     return _response(game.get_user_games, user_id=user_id)
 
@@ -132,6 +134,14 @@ def game__get_user_games(request):
 
 def _params(request):
     return request.GET
+
+def _get_param(params, key, default):
+    value = params.get(key, default)
+
+    if value == default:
+        return default
+    else:
+        return urllib.unquote(value).decode('utf-8')
 
 def _response(fn, **kwargs):
     try:
