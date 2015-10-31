ffd9a6c1459cb863b461122fbef4c5ff3e94226e
yukiwong <yukiwong_ky@hotmail.com>
2015-10-31 12:29:18 -0700

    send guess button

--- client/picturethis/www/js/GuessView.js
+++ client/picturethis/www/js/GuessView.js
@@ -11,4 +11,27 @@ var GuessView = function (service) {
 		return this;
 	};
 	this.initialize();
+
+	this.sendGuess = function(guess) {
+		var retrievedGame =  window.localStorage.getItem('activeGame');
+        var parsedGame = JSON.parse(retrievedGame);
+        var currentWord = parsedGame.curr_word;
+        console.log(currentWord);
+        if (currentWord != guess) {
+        	window.alert("Your guess is incorrect. Try again.");
+        }
+        else {
+        	window.alert("Your guess is correct! Continue!");
+
+			//implementation of sending guess to server, and retrieving updated game (with new word and updated turn) from server
+			//need callback to toPictureView() function
+
+			//not sure if this function will bring user to the photographer view with the new word
+			this.toPictureView();
+        }
+	}
+
+	this.toPictureView = function() {
+		window.location.reload();
+	}
 }
