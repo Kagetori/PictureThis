694fcdf8c678e9846e170198b523929cdb3ac9a3
Kagetori <mt304@hotmail.com>
2015-10-30 21:32:42 -0700

    Implemented sending fake picture

--- client/picturethis/www/js/TakePictureView.js
+++ client/picturethis/www/js/TakePictureView.js
@@ -13,4 +13,15 @@ var TakePictureView = function (word) {
 	};
 
 	this.initialize();
+
+	this.sendPicture = function() {
+		var game = getActiveGame();
+		var user_id = game.user_id;
+		var game_id = game.game_id;
+		showAlert("User&Friend IDs: " + user_id + " " + game_id);
+
+		var api = 'game/send_picture';
+		var params = 'user_id=' + encodeURIComponent(user_id) + '&game_id=' + encodeURIComponent(game_id);
+		var serverCaller = new ServerCaller(api,params,GameParser);
+	};
 }
