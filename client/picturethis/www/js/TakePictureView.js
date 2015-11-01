bd98a06cda1c0cc22da77e6e12be18a25cf3f76a
Kagetori <mt304@hotmail.com>
2015-10-31 21:43:55 -0700

    Added camera functionality but doesn't yet work

--- client/picturethis/www/js/TakePictureView.js
+++ client/picturethis/www/js/TakePictureView.js
@@ -25,3 +25,18 @@ var TakePictureView = function (word) {
 		var serverCaller = new ServerCaller(api,params,GameParser);
 	};
 }
+
+function takePicture() {
+	navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
+        destinationType: Camera.DestinationType.DATA_URL
+    });
+
+    function onSuccess(imageData) {
+        var image = document.getElementById('myImage');
+        image.src = "data:image/jpeg;base64," + imageData;
+    }
+
+    function onFail(message) {
+        debugAlert('Failed because: ' + message);
+    }
+};
