9af03ab1661797cd548dc3d6e8e273d6c5093be9
Meng Tian <mt304@hotmail.com>
2015-11-10 22:54:14 -0800

    deleted debug statements in getFriendListObjects

--- client/picturethis/www/js/FriendListView.js
+++ client/picturethis/www/js/FriendListView.js
@@ -23,34 +23,31 @@ var FriendListView = function (service) {
 }
 // returns parsed friendListObjects
 var getFriendListObjects = function(){
-	debugAlert("got to getFriendListObjects");
 	var user = getUser();
 	var api = 'poll/update';
 	var params = new Array();
 	params['user_id'] = user.id;
 
 	var friendListObjectParser = function(result) {
-		debugAlert("got to PARSER!");
-		console.log(result);
 		var obj = JSON.parse(result);
-		console.log(obj);
 		if (typeof obj.exception === "undefined") {
-		//loop
+
 			var friendList = [];
 			var objects = obj.polls;
 
 			for (var i = 0; i < objects.length; i++) {
-                        var friendListObject = new FriendListObject();
-                        var object = objects[i];
+				var friendListObject = new FriendListObject();
+				var object = objects[i];
 
-						friendListObject.friend_username = object.friend_username;
-						friendListObject.friend_id = object.friend_id;
-						friendListObject.active_game = object.active_game;
-						friendListObject.is_turn = object.is_turn;
-						friendListObject.is_photographer = object.is_photographer;
+				friendListObject.friend_username = object.friend_username;
+				friendListObject.friend_id = object.friend_id;
+				friendListObject.active_game = object.active_game;
+				friendListObject.is_turn = object.is_turn;
+				friendListObject.is_photographer = object.is_photographer;
 
-                        friendList.push(friendListObject);
-                    }
+				friendList.push(friendListObject);
+			}
+
 			return friendList;
 
 		} else {
