5e995ad0fd2905dabc38663b4f9ffe7722cb7416
Karen Guo <karenguo@live.com>
2015-11-16 00:16:10 -0800

    add active and inactive game headers

--- client/picturethis/www/js/FriendListView.js
+++ client/picturethis/www/js/FriendListView.js
@@ -72,25 +72,32 @@ var getFriendListObjects = function(friendTpl){
                 tableul.className = "friends_list";
 
 			if (activeFriends.length != 0) {
+                var activeHeader = document.createElement("li");
+                activeHeader.className = "active_friends";
+                var activeHeaderText = document.createTextNode("Active");
+                activeHeader.appendChild(activeHeaderText);
+                tableul.appendChild(activeHeader);
                 for (i = 0; i < activeFriends.length; i++) {
-                    var tableli = document.createElement("LI");
+                    var tableli = document.createElement("li");
                     tableli.className = "friend_element";
                     friendUserName = activeFriends[i].friend_username;
                     var tabletext = document.createTextNode(friendUserName);
-                    var tablebutton = document.createElement("BUTTON");
-                    tablebutton.className = "play_button";
+                    var tablebutton = document.createElement("button");
 
                     var friendId = activeFriends[i].friend_id;
                     tablebutton.setAttribute("onClick", "play("+friendId.toString()+");");
                     var buttontext;
                     if (activeFriends[i].is_photographer == true) {
-                        buttontext = document.createTextNode("SNAP!");
+                        tablebutton.className = "play_button snap";
+                        buttontext = document.createTextNode("Snap!");
                     }
                     else if (activeFriends[i].is_photographer == false && activeFriends[i].is_turn == true) {
-                        buttontext = document.createTextNode("GUESS!");
+                        tablebutton.className = "play_button guess";
+                        buttontext = document.createTextNode("Guess!");
                     }
                     else {
-                        buttontext = document.createTextNode("WAITING");
+                        tablebutton.className = "play_button waiting";
+                        buttontext = document.createTextNode("Waiting...");
                     }
                     tablebutton.appendChild(buttontext);
                     tableli.appendChild(tabletext);
@@ -99,17 +106,22 @@ var getFriendListObjects = function(friendTpl){
                 }
             }
             if (inactiveFriends.length != 0) {
+                var inactiveHeader = document.createElement("li");
+                inactiveHeader.className = "inactive_friends";
+                var inactiveHeaderText = document.createTextNode("Inactive");
+                inactiveHeader.appendChild(inactiveHeaderText);
+                tableul.appendChild(inactiveHeader);
                 for (i = 0; i < inactiveFriends.length; i++) {
-                    var tableli = document.createElement("LI");
+                    var tableli = document.createElement("li");
                     tableli.className = "friend_element";
                     friendUserName = inactiveFriends[i].friend_username;
                     var tabletext = document.createTextNode(friendUserName);
-                    var tablebutton = document.createElement("BUTTON");
-                    tablebutton.className = "play_button";
+                    var tablebutton = document.createElement("button");
+                    tablebutton.className = "play_button new_game";
 
                     var friendId = inactiveFriends[i].friend_id;
                     tablebutton.setAttribute("onClick", "play("+friendId.toString()+");");
-                    var buttontext = document.createTextNode("START");
+                    var buttontext = document.createTextNode("Start!");
                     tablebutton.appendChild(buttontext);
                     tableli.appendChild(tabletext);
                     tableli.appendChild(tablebutton);
