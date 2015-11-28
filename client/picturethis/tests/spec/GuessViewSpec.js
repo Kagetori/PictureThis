8dce7c06ab84124327e08050b66c4cf7a1d3bc49
Karen Guo <karenguo@live.com>
2015-11-28 12:30:47 -0800

    test random letters

--- client/picturethis/tests/spec/GuessViewSpec.js
+++ client/picturethis/tests/spec/GuessViewSpec.js
@@ -1,5 +1,9 @@
-describe( "randomLetters(numLetters)", function () {
-    it("produces numLetters of random captial letters", function () {
+describe( "randomLetters", function () {
+    it("produce empty string", function () {
         expect(randomLetters(0)).toEqual("");
+    });
+
+    it("produce 4 random letters", function () {
+        expect(randomLetters(4)).toMatch(/^[A-Z][A-Z][A-Z][A-Z]/);
     });
 });
