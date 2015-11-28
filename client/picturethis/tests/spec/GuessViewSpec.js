ba094a4e55e6e2c4d60b341c19b00bbf456ecbbc
Karen Guo <karenguo@live.com>
2015-11-28 12:40:22 -0800

    added tests for GuessViewSpec

--- client/picturethis/tests/spec/GuessViewSpec.js
+++ client/picturethis/tests/spec/GuessViewSpec.js
@@ -1,9 +1,21 @@
 describe( "randomLetters", function () {
-    it("produce empty string", function () {
+    it("returns empty string", function () {
         expect(randomLetters(0)).toEqual("");
     });
 
-    it("produce 4 random letters", function () {
+    it("returns 4 random letters", function () {
         expect(randomLetters(4)).toMatch(/^[A-Z][A-Z][A-Z][A-Z]/);
     });
 });
+
+describe( "shuffleLetters", function() {
+    it ("returns string of same length as input", function() {
+        expect(shuffleLetters("ABCD").length).toEqual(4);
+    });
+});
+
+describe( "stringDiff", function() {
+    it ("returns the difference between two strings", function() {
+        expect(stringDiff("dog", "antidogmatic")).toEqual("antimatic");
+    });
+})
